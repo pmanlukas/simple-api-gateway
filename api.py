e7af352f13cf0692eb521ad8daacce373f83fbba
Lukas Pollmann <pmanlukas@github.com>
2021-04-05 20:36:14 +0000

    fixes

--- api.py
+++ api.py
@@ -1,24 +1,17 @@
 from flask import Flask
 import requests
-import urllib.request
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
+@app.route('/', methods=['GET'])
+@app.route('/hello', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
   
-@app.route('/zen')
+@app.route('/zen', methods=['GET'])
 def get_zen():
     zen = requests.get("https://api.github.com/zen").text
     return zen
-
-@app.route('/octocat')
-def get_octocat():
-    with urllib.request.urlopen("https://api.github.com/octocat") as response:
-        html = response.read().decode("utf8")
-    return html
 
 if __name__ == '__main__':
     app.run()
